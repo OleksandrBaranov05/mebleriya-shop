3b8b16beb9c50cde17cf82bfc243c28aeed26002
Anastasia <akroyl419@gmail.com>
2025-08-09 21:13:25 +0300

    add styles

--- src/js/furniture-list-item.js
+++ src/js/furniture-list-item.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
-const moreBtn = document.querySelector("more-btn")
+const moreBtn = document.querySelector(".more-btn")
 let page = 1;
+let category;
 
 function clearList() { 
     document.querySelector(".card-ul").innerHTML = ''
@@ -11,7 +12,7 @@ async function getImages(category, page = 1) {
   try {
     const response = await axios.get("https://furniture-store.b.goit.study/api-docs/furniture", {
       params: {
-        limit: 15,
+        limit: 8,
         page: page,
         category: category,
       },
@@ -26,12 +27,12 @@ async function getImages(category, page = 1) {
   }
 };
 
-function createList(images) {
+function createList(products) {
     const gallery = document.querySelector(".card-ul");
-    const markup = images.map((image) => {
-        const { images, name, color, price, } = image;
+    const markup = products.map((pr) => {
+        const { images, name, color, price, } = pr;
         return `<li class="card-li">
-        <img src="${images}" alt="${name}" class="card-img">
+        <img src="${images[0]}" alt="${name}" class="card-img">
         <p class="name">${name}</p>
         <div class="colors">${color}</div>
         <p class="price">${price} грн</p>
@@ -42,16 +43,17 @@ function createList(images) {
 }
 
 document.querySelector(".furniture-list").addEventListener("click", async (ev) => {
+  category = ev.target.textContent;
     page = 1;
     clearList()
     try {
-        const images = await getImages(ev.target.value)
-        if (images.items > images.totalItems) {
-            moreBtn.classList.delete("visually-hidden");
+        const images = await getImages(category)
+        if (images.items.length < images.totalItems) {
+            moreBtn.classList.remove("visually-hidden");
         }
         return createList(images.items);
-    } catch {
-        return;
+    } catch(error) {
+        return error;
     }
 
 })
@@ -61,12 +63,13 @@ moreBtn.addEventListener("click", async (ev) => {
   clearList()
   page += 1;
   try {
-    const images = await getImages(ev.target.value, page);
-    if (images.items.length === page * images.items.length) {
+    const images = await getImages(category, page);
+    const totalShown = page * images.items.length;
+    if (totalShown >= images.totalItems) {
       moreBtn.classList.add("visually-hidden");
     }
     return createList(images.items);
-  } catch {
-    return;
+  } catch(error) {
+    return error;
   }
 })
