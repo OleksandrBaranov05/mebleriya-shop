353c199e966cc7c524ee9827caf531ee3dc09acf
Dmitry Stephanik <dmitrystephanik@MacBook-Pro-Dmitry.local>
2025-08-10 18:54:00 +0300

    fix

--- src/js/order-modal.js
+++ src/js/order-modal.js
@@ -1,28 +1,27 @@
+
 const modal = document.getElementById('modal');
 const openBtn = document.querySelector('.open-modal-btn');
 const closeBtn = document.querySelector('.modal__close');
 const overlay = document.querySelector('.modal__overlay');
 const modalContent = document.querySelector('.modal__content');
 
-if (openBtn) {
-  openBtn.addEventListener('click', () => {
-    modal.classList.add('active');
-    modal.setAttribute('aria-hidden', 'false');
-    modalContent.focus();
-  });
-}
+openBtn.addEventListener('click', () => {
+  modal.classList.add('active');
+  modal.setAttribute('aria-hidden', 'false');
+  modalContent.focus();
+});
 
-function closeModal() {
-  modal.classList.remove('active');
-  modal.setAttribute('aria-hidden', 'true');
-  openBtn.focus();
-}
-
-if (closeBtn) closeBtn.addEventListener('click', closeModal);
-if (overlay) overlay.addEventListener('click', closeModal);
+closeBtn.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', event => {
   if (event.key === 'Escape' && modal.classList.contains('active')) {
     closeModal();
   }
 });
+
+function closeModal() {
+  modal.classList.remove('active');
+  modal.setAttribute('aria-hidden', 'true');
+  openBtn.focus();
+}
